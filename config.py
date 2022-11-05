6c72b74ab90e90febfb189cd843bd6719ccd5458
SemyonC20 <semenmalahov906@gmail.com>
2022-11-05 21:26:19 +0700

    Add requirements for heroku

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 
-TOKEN = os.environ['TOKEN']
+TOKEN = os.environ['TOKEN'] if os.environ['TOKEN'] is not '' or os.environ['TOKEN'] is not None else '5757913178:AAFenicSGytVqJ5zzY1n3GAXs7Y0tKlMZ1o'
