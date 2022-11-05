81dfaec177c58b5cbe35a275d2bbe4b6c7d2c43f
SemyonC20 <semenmalahov906@gmail.com>
2022-11-05 21:34:47 +0700

    Add token get from env

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
 
-TOKEN = os.environ['TOKEN'] if os.environ['TOKEN'] is not '' or os.environ['TOKEN'] is not None else '5757913178:AAFenicSGytVqJ5zzY1n3GAXs7Y0tKlMZ1o'
+try:
+    TOKEN = os.environ['TOKEN']
+except KeyError:
+    TOKEN = '5757913178:AAFenicSGytVqJ5zzY1n3GAXs7Y0tKlMZ1o'
